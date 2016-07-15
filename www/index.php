26090069df9c9dc117481261616e0a4826bad76a
Тема <pomoshhuk@gmail.com>
2016-07-15 16:52:53 +0300

    nanana

---
+++ www/index.php
@@ -0,0 +1,31 @@
+﻿<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+    <title>Главная</title>
+	<link rel="stylesheet" href="libs/bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+ <link rel="stylesheet" href="css/fonts.css" />
+	<link rel="stylesheet" href="css/style.css" />
+  <script src="libs/jquery/jquery-2.1.4.min.js"></script>
+<script src="libs/angular.js"></script>
+	<script src="js/common.js"></script>
+</head>
+<body>
+	<a class="admin">Admin</a>
+		<div class="container">
+		<div class="row header">
+				<div class="col-md-offset-2 col-md-8 text-center">
+					<h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h1>
+				</div>
+			</div>
+			<div class="row degustation">
+				<div class="col-md-offset-2 col-md-8 text-center">
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem enim repellendus aliquam voluptates quam, error corporis deleniti quo tenetur assumenda ea, excepturi nobis explicabo? Nostrum dignissimos animi eligendi id totam.</p>
+					<button class="btn">Дегустация</button>
+				</div>
+			</div>
+		</div>
+</body>
+</html>
