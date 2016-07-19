d01fd34fbaefe31a9efbd1aba5bf78f2e42e6ff8
Hecmatyar <dmitriy.kushnir@inbox.ru>
2016-07-19 11:27:35 +0300

    new bg

--- www/index.php
+++ www/index.php
@@ -13,12 +13,9 @@
 	<script src="js/common.js"></script>
 </head>
 <body>
-
-	<div class="bg">			
-		
-		<div class="container">		
-		<a class="smoothly" href="admin.php">Администратор</a>
-		<div class="vertical-align"	>
+	<div class="bg">
+	<a class="smoothly" href="admin.php">Администратор</a>	
+		<div class="container">					
 			<div class="row header">			
 				<div class="col-md-12 col-xs-offset-0 col-xs-12 text-center">					
 					<h1>Дегустация</h1>
@@ -27,14 +24,14 @@
 			</div>
 			<div class="row degustation">
 				<div class="col-md-offset-2 col-md-8 col-xs-offset-1 col-xs-10 text-center">
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem enim repellendus aliquam voluptates quam, error corporis deleniti quo tenetur.					
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem enim repellendus aliquam voluptates quam, error corporis deleniti quo tenetur.				
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem enim repellendus aliquam voluptates quam, error corporis deleniti quo tenetur.				
 					</p>
 					<button class="start-btn smoothly" onClick='location.href="taster.php"'>
 					Начать дегустацию
 					</button>
 				</div>
-			</div>		
-		</div>	
+			</div>
 		</div>
 	</div>
 </body>
