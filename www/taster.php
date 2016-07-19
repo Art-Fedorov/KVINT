9fb5faebecae4bd1f5df2ad5ff9aa608e0a5e218
Тема <pomoshhuk@gmail.com>
2016-07-19 17:03:03 +0300

    PHP GOD

--- www/taster.php
+++ www/taster.php
@@ -1,9 +1,9 @@
-﻿<?php /*
+﻿<?php 
 	$conn = oci_connect('TASTING', '1111', 'ora2.kvint.md/UNIACC', 'CL8MSWIN1251');
 					if (!$conn) {
     				$e = oci_error();
     				trigger_error(htmlentities($e['message'], ENT_QUOTES, 'cp1251'), E_USER_ERROR);
-					}*/
+					}
  ?>
 
 <!DOCTYPE html>
@@ -11,7 +11,7 @@
 <head>
 <meta charset="windows-1251" />
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
-    <title>Дигустация</title>
+    <title>Дегустация</title>
 	<link rel="stylesheet" href="libs/bootstrap/css/bootstrap.min.css">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
  	<link rel="stylesheet" href="css/fonts.css" />
@@ -23,15 +23,24 @@
 	<script type="text/javascript">
 	$(function(){
 		$('#exit').click(function(){
-			$('.cd-wrapper').css("display","none");
-		})
+			$('.cd-wrapper').hide();
+		});
 
 		$('#start-degustation').click(function(){
-			$('.cd-wrapper').css("display","block");
-		})
+			$('.cd-wrapper').show();
+		});
+
+		$('div.list p').click(function(){
+			$('#start-degustation').text($(this).text());
+			$('.vibortext').hide();
+			$('#exit').click();
+		});
 	})
-
-
+	function getyear(){
+		var d = new Date();
+		var n = d.getFullYear();
+		return n;
+	}
 	</script>
 </head>
 <body>
@@ -40,18 +49,61 @@
 		<div class="container">	
 			<div class="row header">			
 				<div class="col-md-12 col-xs-offset-0 col-xs-12 text-center">
-					<h1>Выберите</h1>
-					<p>свое имя и фамилию из списка</p>
+					
 				</div>
 			</div>
 			<div class="row degustation">
 				<div class="col-md-offset-2 col-md-8 col-xs-offset-1 col-xs-10 text-center">
+					<div class="vibortext"><h1>CHOOSE YOUR NAME</h1>
+					<!--<p>свое имя и фамилию из списка</p>-->
+					</div>
 					<button class="choose-send smoothly" name="StartDeg" id="start-degustation">
-					Начать дегустацию
+					Выбрать имя
 					</button>
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem enim repellendus aliquam voluptates quam, error corporis deleniti quo tenetur.				
-					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem enim repellendus aliquam voluptates quam, error corporis deleniti quo tenetur.				
-					</p>
+					
+					<form action="taster.php">
+					<div class="taster">
+
+					</div>
+					
+					<nav class="cognacgroup">
+						<ul>
+							<li><a href="taster.php?code=A">A</a></li>
+							<li><a href="taster.php?code=B">B</a></li>
+							<li><a href="taster.php?code=C">C</a></li>
+							<li><a href="taster.php?code=D">D</a></li>
+							<li><a href="taster.php?code=E">E</a></li>
+							<li><a href="taster.php?code=F">F</a></li>
+							<li><a href="taster.php?code=G">G</a></li>
+							<li><a href="taster.php?code=K">K</a></li>
+						</ul>
+					</nav>
+
+
+					<?php	
+					if (isset($_GET['code']))
+					{
+						$code=$_GET['code'];
+						$query='SELECT COGNAC_CODE,COGNAC_AGE FROM TAST_COGNAC WHERE COGNAC_CAPTION=63 AND COGNAC_CODE like \''.$code.'%\' ORDER BY COGNAC_CODE';
+						$stid = oci_parse($conn,$query );
+						oci_execute($stid);
+						echo "<div class=\"list\">\n";
+						$i=0;
+						while ($row = oci_fetch_array($stid, OCI_ASSOC+OCI_RETURN_NULLS)) {
+						    echo "<p name=\"p".$i++."\">\n";
+						    foreach ($row as $item) {
+						        echo $item !== null ? htmlentities($item, ENT_QUOTES, 'cp1251') : "";
+						    }
+						    echo "</p>\n";
+						    echo '<span>Прозрачность</span><input type="number" name="opacity" required min="0" max="0.5 step="0.05">';
+
+					}
+					echo "</div>\n";
+					}
+					
+					?>
+					</form>
+
 				</div>
 			</div>			
 		</div>
@@ -59,9 +111,9 @@
 	
 
 	<div class="cd-wrapper">
-		<button class="choose-send smoothly" name="Exit" id="exit">
-					Отменить 					</button>
-			<?php	/*
+		<button class="choose-send smoothly" name="Exit" id="exit">Отменить</button>
+
+			<?php	
 					$stid = oci_parse($conn, 'SELECT MAN_FIO FROM tAst_MAN WHERE MAN_CAPTION=63');
 					oci_execute($stid);
 					echo "<div class=\"list\">\n";
@@ -74,7 +126,7 @@
 					    }
 					    echo "</p>\n";
 					}
-					echo "</div>\n";*/
+					echo "</div>\n";
 					?>
 	</div> <!-- .cd-wrapper -->	
 </body>
