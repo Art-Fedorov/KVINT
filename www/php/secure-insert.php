73c15196419feecc32e863d23594d11d50e58331
Тема <pomoshhuk@gmail.com>
2016-07-25 17:07:06 +0300

    insert or update commit2

--- www/php/secure-insert.php
+++ www/php/secure-insert.php
@@ -17,19 +17,31 @@
 								'typicality'=>$_POST['typicality'.$i]);
 
 								//Проверка на то, заполнял ли дегустатор этот коньяк
-								$q='SELECT RATING_MAN FROM TAST_RATING WHERE RATING_MAN='.$taster.'AND RATING_COGNAC='.$array[$i]['cognac_id'].'AND RATING_CAPTION=(SELECT MAX(CAPTION_ID) FROM TAST_CAPTION)';
+								$q='SELECT COUNT(RATING_MAN) FROM TAST_RATING WHERE RATING_MAN='
+								.$taster.'AND RATING_COGNAC='
+								.$array[$i]['cognac_id'].
+								'AND RATING_CAPTION=(SELECT MAX(CAPTION_ID) FROM TAST_CAPTION)';	
 								$s = oci_parse($conn,$q);
 
 								oci_execute($s);
-								$count=oci_num_rows($s);
-								/*while ($row = oci_fetch_array($s)) {
-									$count=htmlentities($row[0], ENT_QUOTES, 'cp1251');
-								}*/
-								debug($count);
-								debug($q);
+								$count=0;
+								while ($row = oci_fetch_array($s)) {
+									$count++;
+								}
+								
 								
 								if ($count==0) {
 									$query = 'INSERT INTO TAST_RATING (RATING_MAN,RATING_COGNAC,RATING_CAPTION,RATING_POINT,RATING_OPACITY,RATING_COLOR,RATING_BOUQUET,RATING_TASTE,RATING_TYPICALITY,RATING_NOTE) VALUES ('.$taster.','.$array[$i]['cognac_id'].',(SELECT MAX(CAPTION_ID) FROM TAST_CAPTION),'.$array[$i]['mainpoint'].','.$array[$i]['opacity'].','.$array[$i]['color'].','.$array[$i]['bouquet'].','.$array[$i]['taste'].','.$array[$i]['typicality'].',\''.$array[$i]['note'].'\')';
+									$stid = oci_parse($conn,$query);
+									$i++;
+									oci_execute($stid);
+								}
+								else
+								{
+									$query = 'UPDATE TAST_RATING set RATING_MAN='.$taster.', RATING_COGNAC='.$array[$i]['cognac_id'].', RATING_CAPTION=(SELECT MAX(CAPTION_ID) FROM TAST_CAPTION), RATING_POINT='.$array[$i]['mainpoint'].', RATING_OPACITY='.$array[$i]['opacity'].', RATING_COLOR='.$array[$i]['color'].', RATING_BOUQUET='.$array[$i]['bouquet'].', RATING_TASTE='.$array[$i]['taste'].', RATING_TYPICALITY='.$array[$i]['typicality'].', RATING_NOTE=\''.$array[$i]['note'].'\' WHERE RATING_MAN='
+								.$taster.'AND RATING_COGNAC='
+								.$array[$i]['cognac_id'].
+								'AND RATING_CAPTION=(SELECT MAX(CAPTION_ID) FROM TAST_CAPTION)';
 									$stid = oci_parse($conn,$query);
 									$i++;
 									oci_execute($stid);
