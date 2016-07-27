ddd268a684fd22922eb62568baee8531df93212d
Тема <pomoshhuk@gmail.com>
2016-07-27 13:59:39 +0300

    sasd

---
+++ www/js/common.js
@@ -0,0 +1,12 @@
+var action;
+$(function(){
+	$('#right-column-button-add').click(function(){
+		action=1;
+	});
+	$('#right-column-button-re').click(function(){
+		action=2;
+	});
+	$('#right-column-button-del').click(function(){
+		action=3;
+	});
+})
--- www/js/select-table-tr.js
+++ www/js/select-table-tr.js
@@ -1,17 +1,54 @@
 var tr_id;
+var tr_help;
 var table;
 var id_row;
   $(document).ready(function(){
     $('tr').click(function(){
         $('tr').removeClass();
         $(this).addClass('selected');
-        tr_id = $(this).data('value');        
+        tr_id = $(this).data('value');
+        tr_help = $(this).attr('data-value-help');        
     });
     $('.button-ok').click(function(){
           delete_row();
         });
+    $('#right-column-button-re').click(function(){
+          //delete_row();
+          Show_float_window();
+          $('tr[data-value='+tr_id+']').find('td').each(function(key,val){
+            alert(key+" "+$(val).text());
+            
+          });
+          //alert(action);
+          //alert(tr_id+" "+tr_help);
+        });
   });
-
+function Show(){    
+    var query = window.location.search.substring(1);
+    var vars = query.split("&");     
+    var l = vars[0].split("=");
+    var codetable = l[1];
+    if (codetable=='1'){
+        $("#popup1").show();
+        table_add = 'TAST_CAPTION';
+    } else if (codetable=='2'){
+        $("#popup2").show();
+        table_add = 'TAST_GROUP';
+    } else if (codetable=='3'){
+        $("#popup3").show();
+        table_add = 'TAST_PRIZE';
+    } else if (codetable=='4'){
+        $("#popup4").show();
+        table_add = 'TAST_MAN';
+    } else if (codetable=='5'){
+        $("#popup5").show();
+        table_add = 'TAST_COGNAC';
+    } else if (codetable=='6'){
+        $("#popup6").show();
+        table_add = 'TAST_RATING';
+    }
+    else  $("#popup1").show(); 
+}
 function Show_tr_id(){
   alert(tr_id);
 }
