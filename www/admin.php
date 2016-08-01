747acb44522456c3921c50ca8ab30dd39e79615a
Тема <pomoshhuk@gmail.com>
2016-08-01 17:12:36 +0300

    123

--- www/admin.php
+++ www/admin.php
@@ -4,7 +4,7 @@
 <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
     <title>Справочник</title>
-  <link rel="stylesheet" href="libs/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="libs/bootstrap/css/bootstrap.css">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
   <link rel="stylesheet" href="css/admin-page-catalog.css" />
   <link rel="stylesheet" href="css/admin-page-rating.css" />
