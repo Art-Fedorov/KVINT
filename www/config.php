a748f087c57bba42f8293e1d2601fec69db3ad73
Hecmatyar <dmitriy.kushnir@inbox.ru>
2016-08-02 14:07:34 +0300

    config

--- www/config.php
+++ www/config.php
@@ -1,21 +1,20 @@
 <?php
-
+class Config {	
+	protected  $admin_password = 'tast';	
+	protected  $admin_login = 'kvint'; 
 	
-
-	function control_admin ($log, $pass){
-		$admin_password = 'tast';
-	$admin_login = 'kvint';
-		if ($admin_password == $pass && $admin_login == $log) echo '1';
+	function control_admin ($log, $pass){		
+		if ($this->admin_password == $pass && $this->admin_login == $log) 
+			echo '1';
 		else echo '0';
-		//echo $log." ".$pass." ||| ".$admin_password." ".$admin_login;
 	}
-
+}
 if (!empty($_POST))
 	{		
 		$log=$_POST['login'];
 		$pass=$_POST['password'];
-		//$control = new Config();
-    control_admin($log, $pass);
+		$control = new Config();
+    $control -> control_admin($log, $pass);
 	}
 
 ?>
