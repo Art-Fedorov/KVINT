be1060aed61a165d97f312e70feaac0c4f952620
Тема <pomoshhuk@gmail.com>
2016-08-03 15:10:49 +0300

    ZXZX

--- www/reports/report1.php
+++ www/reports/report1.php
@@ -14,11 +14,11 @@ use Dompdf\Exception;
     /*$html='<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8">
   <title>Список образцов</title><link rel="stylesheet" href="../libs/bootstrap/css/bootstrap.min.css"><!--<link rel="stylesheet" type="text/css" href="../css/report-page.css">--><link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css"><script src="../libs/jquery/jquery-2.1.4.min.js"></script></head><body><header> <div style="width:600px; margin:0 auto; text-align: center;"><p><span>СПИСОК ОБРАЗЦОВ</span><br>представленных на Международном Конкурсе коньяков<br> "KVINT 2017" г. Тирасполь</p></div>  </header>';*/
     
-    /*for ($i = 2; $i-1 < oci_num_fields($stid); $i++) {
-    $html.= "<tr>";
+    for ($i = 2; $i-1 < oci_num_fields($stid); $i++) {
+    $html.= "<th>";
          $html.= oci_field_name($stid,$i);            
-     $html.= "</tr>";
-    }*/
+     $html.= "</th>";
+    }
     while ($row = oci_fetch_array($stid)) {
        $html.= "<tr>";
       for ($i = 1; $i < oci_num_fields($stid); $i++) {
@@ -30,15 +30,22 @@ use Dompdf\Exception;
     }
     $html.= "</table></div>\n";  
     $html.="</html></body>";
-    require_once("dompdf/autoload.inc.php");
+    //$html=iconv("ISO-8859-1","windows-1251",$html);
+    /*iconv_set_encoding("internal_encoding", "UTF-8");
+    iconv_set_encoding("input_encoding", "UTF-8");
+    iconv_set_encoding("output_encoding", "UTF-8");*/
+    /*require_once("dompdf/autoload.inc.php");
     $dompdf = new DOMPDF();// Создаем обьект
     $dompdf->load_html($html); // Загружаем в него наш html код
-    $dompdf->set_paper('A4','potrait');
+    $dompdf->set_paper('A4','album');
     $dompdf->render(); // Создаем из HTML PDF
     //$dompdf->stream('mypdf.pdf'); // Выводим результат (скачивание)
-    $output = $dompdf->output();
-    file_put_contents('Brochure.pdf', $output);
-    //echo $htm;
+    $output = $dompdf->output();*/
+    //file_put_contents('Brochure.pdf', $output);
+    file_put_contents('Brochure.html', $html);
+
+    //echo var_dump(iconv_get_encoding('all'));
+    echo $html;
     
 
   } 
